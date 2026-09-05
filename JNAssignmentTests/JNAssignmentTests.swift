7fa60addba33ebbd1d3215cf17c190c6db3a9512
SeungHyun Hong <whi7ehyun@gmail.com>
2026-09-05 12:36:56 +0900

    [Init] 프로젝트 기본 세팅 (#1)
    ## 설명
    
    - iOS 17부터 제공되는 Observation 프레임워크를 활용하고, 여러 주요 국내 앱의 최소 지원 버전이 iOS 17
    이상인 점을 고려해 최소 지원 버전을 iOS 17로 설정
    - Swift 6로 language mode 적용
    - mise로 swiftformat, swiftlint 관리 및 룰 적용
    
    - Sources와 Resources로 프로젝트 구조 간단히 분리
    ```text
    JNAssignment
    ├── Resources
    │   └── Assets.xcassets
    └── Sources
        ├── App
        │   └── JNAssignmentApp.swift
        └── ContentView.swift
    ```

---
+++ JNAssignmentTests/JNAssignmentTests.swift
@@ -0,0 +1,12 @@
+//
+//  JNAssignmentTests.swift
+//  JNAssignmentTests
+//
+//  Created by whitehyun on 9/4/26.
+//
+
+import Testing
+@testable import JNAssignment
+
+struct JNAssignmentTests {
+}
