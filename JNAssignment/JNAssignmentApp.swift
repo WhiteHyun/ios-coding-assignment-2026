ffa4407775508e5adea49e32e74aa4c51a1beed7
SeungHyun Hong <whi7ehyun@gmail.com>
2026-09-05 12:34:33 +0900

    init: Initial Commit

---
+++ JNAssignment/JNAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  JNAssignmentApp.swift
+//  JNAssignment
+//
+//  Created by whitehyun on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct JNAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
