a8167ca1065f58d86ca5f49046cd1037aec85790
SeungHyun Hong <whi7ehyun@gmail.com>
2026-09-05 12:37:03 +0900

    feat(network): 헤더 생성 메서드와 배열 리터럴 지원 추가

--- Core/Sources/Networker/Foundation/HTTPHeader.swift
+++ Core/Sources/Networker/Foundation/HTTPHeader.swift
@@ -5,12 +5,40 @@
 //  Created by whitehyun on 9/5/26.
 //
 
+// MARK: - HTTPHeader
+
+/// HTTP 헤더를 나타냅니다
 public struct HTTPHeader: Sendable, Hashable {
-  public let key: String
-  public let value: String
+  /// header 이름
+  let key: String
+
+  /// header에 대응되는 값
+  let value: String
 
   public init(key: String, value: String) {
     self.key = key
     self.value = value
+  }
+}
+
+extension HTTPHeader {
+  public static func accept(_ value: String) -> Self {
+    HTTPHeader(key: "Accept", value: value)
+  }
+
+  public static func contentType(_ value: String) -> Self {
+    HTTPHeader(key: "Content-Type", value: value)
+  }
+
+  public static func authorization(bearer token: String) -> Self {
+    HTTPHeader(key: "Authorization", value: "Bearer \(token)")
+  }
+}
+
+// MARK: CustomStringConvertible
+
+extension HTTPHeader: CustomStringConvertible {
+  public var description: String {
+    "\(key): \(value)"
   }
 }
--- Core/Sources/Networker/Foundation/HTTPHeaders.swift
+++ Core/Sources/Networker/Foundation/HTTPHeaders.swift
@@ -5,14 +5,25 @@
 //  Created by whitehyun on 9/4/26.
 //
 
+// MARK: - HTTPHeaders
+
 public struct HTTPHeaders: Sendable, Hashable {
   public let headers: [HTTPHeader]
 
-  public init(_ headers: [HTTPHeader] = []) {
+  public init(headers: [HTTPHeader]) {
     self.headers = headers
   }
 
   var dictionary: [String: String] {
-    Dictionary(uniqueKeysWithValues: headers.map { ($0.key, $0.value) })
+    let headersTuple = headers.map { ($0.key, $0.value) }
+    return Dictionary(uniqueKeysWithValues: headersTuple)
+  }
+}
+
+// MARK: ExpressibleByArrayLiteral
+
+extension HTTPHeaders: ExpressibleByArrayLiteral {
+  public init(arrayLiteral elements: HTTPHeader...) {
+    self.init(headers: elements)
   }
 }
